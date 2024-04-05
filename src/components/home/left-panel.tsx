0424fcda8213cf605ecfbc4a940687cf2a2eae3d
phamdung2209 <phamdung.22092003@gmail.com>
2024-04-05 23:56:48 +0700

    Add new dependencies and update layout component in the app

---
+++ src/components/home/left-panel.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LeftPanel = () => {
+    return <div>LeftPanel</div>
+}
+
+export default LeftPanel
---
+++ src/components/home/right-panel.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RightPanel = () => {
+    return <div>RightPanel</div>
+}
+
+export default RightPanel
