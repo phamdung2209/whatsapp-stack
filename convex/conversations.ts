4763bd0905c2fa5e161d7e0dadd6f5c2231e8821
phamdung2209 <phamdung.22092003@gmail.com>
2024-04-20 15:24:00 +0700

    feat: get my conversations query added

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from 'convex/values'
-import { mutation } from './_generated/server'
+import { mutation, query } from './_generated/server'
 
 export const createConversation = mutation({
     args: {
@@ -47,4 +47,53 @@ export const createConversation = mutation({
 
 export const generateUploadUrl = mutation(async (ctx) => {
     return await ctx.storage.generateUploadUrl()
+})
+
+export const getMyConversations = query({
+    args: {},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity()
+        if (!identity) throw new ConvexError('Unauthorized')
+
+        const user = await ctx.db
+            .query('users')
+            .withIndex('by_tokenIdentifier', (q) =>
+                q.eq('tokenIdentifier', identity.tokenIdentifier.replace('https://', '')),
+            )
+            .unique()
+
+        if (!user) throw new ConvexError('User not found')
+
+        const conversations = await ctx.db.query('conversations').collect()
+
+        const myConversations = conversations.filter((conversation) => conversation.participants.includes(user._id))
+        const conversationWithDetails = await Promise.all(
+            myConversations.map(async (conversation) => {
+                let userDetails = {}
+                if (!conversation.isGroup) {
+                    const otherUserId = conversation.participants.find((participant) => participant !== user._id)
+                    const userProfile = await ctx.db
+                        .query('users')
+                        .filter((q) => q.eq(q.field('_id'), otherUserId))
+                        .take(1)
+                    console.log('userProfile', userProfile)
+
+                    userDetails = userProfile[0]
+                }
+
+                const lastMessage = await ctx.db
+                    .query('messages')
+                    .filter((q) => q.eq(q.field('conversation'), conversation._id))
+                    .order('desc')
+                    .take(1)
+
+                return {
+                    ...userDetails,
+                    ...conversation,
+                    lastMassage: lastMessage[0] || null,
+                }
+            }),
+        )
+        return conversationWithDetails
+    },
 })
--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -1,14 +1,16 @@
 import { ListFilter, LogOut, Search } from 'lucide-react'
 import { Input } from '../ui/input'
 import ThemeSwitch from './theme-switch'
-import { conversations } from '~/dummy-data/db'
 import Conversation from './conversation'
 import { UserButton } from '@clerk/nextjs'
 import UserListDialog from './user-list-dialog'
-import { useConvexAuth } from 'convex/react'
+import { useConvexAuth, useQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
 
 const LeftPanel = () => {
     const { isAuthenticated } = useConvexAuth()
+    const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : 'skip')
+    console.log('getMyConversations', conversations)
 
     return (
         <div className="w-1/4 border-gray-600 border-r">
